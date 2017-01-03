afce849da0515d99ccbfc52b88c67a909e0e424f
陈志勇 <czy@czydemacbook-pro.local>
2017-01-03 22:55:11 +0800

    修改AppDelegate

--- 103--baisibudejie/103--baisibudejie/Classes/Other/AppDelegate.m
+++ 103--baisibudejie/103--baisibudejie/Classes/Other/AppDelegate.m
@@ -16,7 +16,17 @@
 
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-    // Override point for customization after application launch.
+    
+    // 1.创建窗口
+    self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
+    
+    // 2.添加根控制器
+    UITabBarController *tabBarVC = [[UITabBarController alloc] init];
+    self.window.rootViewController = tabBarVC;
+    
+    // 3.显示窗口
+    [self.window makeKeyAndVisible];
+    
     return YES;
 }
 
